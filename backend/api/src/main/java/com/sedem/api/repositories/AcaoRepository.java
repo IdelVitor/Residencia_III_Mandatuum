9c475f38efaad7483158b59ba976faf81139ca8a
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2025-10-15 20:47:45 -0300

    criando acaorepository

--- backend/api/src/main/java/com/sedem/api/repositories/AcaoRepository.java
+++ backend/api/src/main/java/com/sedem/api/repositories/AcaoRepository.java
@@ -1,9 +1,15 @@
 package com.sedem.api.repositories;
 
 import com.sedem.api.models.Acao;
+import com.sedem.api.models.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AcaoRepository extends JpaRepository<Acao, Long> {
+    Optional<Acao> findById(Long id);
+    List<Acao> findAll();
 }
