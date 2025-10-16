478e747976356dcd9a8d5878ade73df370487505
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2025-10-15 21:15:34 -0300

    criando todas as repositories das entidades contato, tarefa, registro, useraccess e permission

--- backend/api/src/main/java/com/sedem/api/repositories/RegistroFinanceiroRepository.java
+++ backend/api/src/main/java/com/sedem/api/repositories/RegistroFinanceiroRepository.java
@@ -1,9 +1,18 @@
 package com.sedem.api.repositories;
 
+import com.sedem.api.models.Contato;
 import com.sedem.api.models.RegistroFinanceiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RegistroFinanceiroRepository extends JpaRepository<RegistroFinanceiro, Long> {
+    Optional<RegistroFinanceiro> findById(Long id);
+
+    List<RegistroFinanceiro> findAll();
+
+    void deleteById(Long id);
 }
--- backend/api/src/main/java/com/sedem/api/repositories/TarefaRepository.java
+++ backend/api/src/main/java/com/sedem/api/repositories/TarefaRepository.java
@@ -1,9 +1,18 @@
 package com.sedem.api.repositories;
 
+import com.sedem.api.models.Contato;
 import com.sedem.api.models.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    Optional<Tarefa> findById(Long id);
+
+    List<Tarefa> findAll();
+
+    void deleteById(Long id);
 }
